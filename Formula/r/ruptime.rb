e2fc695f1c896675321b6087e012174fd43914ec
Alex Myczko <alex@aiei.ch>
2024-02-27 10:18:07 +0100

    ruptime 1.8 (new formula)

---
+++ Formula/r/ruptime.rb
@@ -0,0 +1,31 @@
+class Ruptime < Formula
+  desc "poor man’s ruptime"
+  homepage "https://wedonthaveaprivacyproblem.com"
+  url "https://github.com/alexmyczko/ruptime/archive/refs/tags/1.8.tar.gz"
+  sha256 "c7115c34e11d79d1d0e966516818f3e5f7a0417aadb6cdddfb415cb103f688eb"
+  license "AGPL-3.0"
+
+  depends_on "xz"
+  depends_on "memtester"
+
+  def install
+    bin.install "ruptime"
+    #bin.install "ruptimed"
+    bin.install_symlink "ruptime" => "rsw"
+    bin.install_symlink "ruptime" => "rhw"
+    bin.install_symlink "ruptime" => "runame"
+    bin.install_symlink "ruptime" => "rbench"
+    bin.install_symlink "ruptime" => "rboot"
+    bin.install_symlink "ruptime" => "rdisk"
+    bin.install_symlink "ruptime" => "rload"
+    bin.install_symlink "ruptime" => "rnet"
+    bin.install_symlink "ruptime" => "rac"
+    bin.install_symlink "ruptime" => "rwho"
+    man1.install "man/ruptime.1"
+    #man8.install "man/ruptimed.8"
+  end
+
+  test do
+    system "false"
+  end
+end
